e06c2e94fa7b8b48feb358243fca33dc3471d6ce
minkiko@gmail.com <minkiko@gmail.com>
2023-08-12 12:00:39 -0500

    fixed wording on some index.js files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,6 +1,6 @@
 // import models
-const Product = require('./product');
-const Category = require('./category');
+const Product = require('./Product');
+const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
