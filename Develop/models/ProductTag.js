75c714515270a345aaa1249e5d29a426c6398190
minkiko@gmail.com <minkiko@gmail.com>
2023-08-12 11:36:08 -0500

    polished changes to models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
@@ -6,27 +7,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define 
-    
-    id: {
-        type : DataTypes.INTEGER,
-        primaryKey : true,
-        autoIncrement: true
-       },
-       product_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          'model': 'product',
-          'key': 'id'
-        }
-       },
-       tag_id: {
-        type: DataTypes.INTEGER,
-        references : {
-          'model' : 'tag',
-          'key': 'id'
-        }
-       }
+   id: {
+    type : DataTypes.INTEGER,
+    primaryKey : true,
+    autoIncrement: true
+   },
+   product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      'model': 'product',
+      'key': 'id'
+    }
+   },
+   tag_id: {
+    type: DataTypes.INTEGER,
+    references : {
+      'model' : 'tag',
+      'key': 'id'
+    }
+   }
   },
   {
     sequelize,
